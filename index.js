6f5174ad82d28045ad42f4bead139c257d6a3aa3
flolep2607 <flo.lep2607@gmail.com>
2022-02-26 17:07:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { readFileSync, writeFileSync, readdirSync } = require("fs");
+const { readFileSync, writeFileSync, readdirSync, statSync } = require("fs");
 const { join } = require("path");
 const core = require("@actions/core");
 const $ = require("@k3rn31p4nic/google-translate-api");
@@ -16,63 +16,90 @@ const toAst = (markdown) => {
 const toMarkdown = (ast) => {
   return unified().use(stringify).stringify(ast);
 };
+const recFindByExt=(base,files,result) => {
+    files = files || readdirSync(base) 
+    result = result || [] 
+    files.forEach( 
+        function (file) {
+            var newbase = path.join(base,file)
+            if (statSync(newbase).isDirectory())
+            {
+                result = recFindByExt(newbase,readdirSync(newbase),result)
+            }
+            else
+            {
+                if (file.endsWith('.md') && !file.match(/\.[a-z]{2}(-[A-Z]{2})?\.md$/m)) // already translated
+                {
+                    result.push(newbase);
+                } 
+            }
+        }
+    )
+    return result
+}
 
 const mainDir = ".";
-let README = readdirSync(mainDir).includes("readme.md")
-  ? "readme.md"
-  : "README.md";
-const lang = core.getInput("LANG") || "zh-CN";
-const readme = readFileSync(join(mainDir, README), { encoding: "utf8" });
-const readmeAST = toAst(readme);
-console.log("AST CREATED AND READ");
+let FILES = recFindByExt(mainDir);
+const lang = core.getInput("LANG") || "en";
+//let README = readdirSync(mainDir).includes("readme.md")? "readme.md": "README.md";
 
-let originalText = [];
 
-visit(readmeAST, async (node) => {
-  if (node.type === "text") {
-    originalText.push(node.value);
-    node.value = (await $(node.value, { to: lang })).text;
+
+  async function writeToFile(file_part,translatedText) {
+    await Promise.all(translatedText);
+    writeFileSync(
+      join(mainDir, `${file_part}.${lang}.md`),
+      toMarkdown(readmeAST),
+      "utf8"
+    );
+    console.log(`${file_part}.${lang}.md written`);
   }
-});
 
-const translatedText = originalText.map(async (text) => {
-  return (await $(text, { to: lang })).text;
-});
-
-async function writeToFile() {
-  await Promise.all(translatedText);
-  writeFileSync(
-    join(mainDir, `README.${lang}.md`),
-    toMarkdown(readmeAST),
-    "utf8"
-  );
-  console.log(`README.${lang}.md written`);
-}
-
-async function commitChanges(lang) {
-  console.log("commit started");
-  await git.add("./*");
-  await git.addConfig("user.name", "github-actions[bot]");
-  await git.addConfig(
-    "user.email",
-    "41898282+github-actions[bot]@users.noreply.github.com"
-  );
-  await git.commit(
-    `docs: Added README."${lang}".md translation via https://github.com/dephraiim/translate-readme`
-  );
-  console.log("finished commit");
-  await git.push();
-  console.log("pushed");
-}
-
-async function translateReadme() {
-  try {
-    await writeToFile();
-    await commitChanges(lang);
-    console.log("Done");
-  } catch (error) {
-    throw new Error(error);
+  async function commitChanges(file_part,lang) {
+    console.log("commit started");
+    await git.add("./*");
+    await git.addConfig("user.name", "github-actions[bot]");
+    await git.addConfig(
+      "user.email",
+      "41898282+github-actions[bot]@users.noreply.github.com"
+    );
+    await git.commit(
+      `docs: Added ${file_part}."${lang}".md translation via https://github.com/dephraiim/translate-readme`
+    );
+    console.log("finished commit");
   }
-}
 
-translateReadme();
+  async function translateReadme() {
+    try {
+      for(file of FILES){
+        const file_part = file.replace('.md','');
+        const readme = readFileSync(join(mainDir, file), { encoding: "utf8" });
+        const readmeAST = toAst(readme);
+        console.log("AST CREATED AND READ");
+
+        let originalText = [];
+
+        visit(readmeAST, async (node) => {
+          if (node.type === "text") {
+            originalText.push(node.value);
+            node.value = (await $(node.value, { to: lang })).text;
+          }
+        });
+
+        const translatedText = originalText.map(async (text) => {
+          return (await $(text, { to: lang })).text;
+        });
+        await writeToFile(file_part,translatedText);
+        await commitChanges(file_part,lang);
+        console.log("commit",file_part,lang);
+      }
+      await git.push();
+      console.log("pushed");
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  translateReadme();
+
+
